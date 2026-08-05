5988d38f332bb4a7c8aa9ff491b06276e4b97c62
Mohammed Alabsi <mohammedalialabsy@gmail.com>
2026-08-05 19:14:37 +0300

    تم انجاز عملية تسجيل الخروج بنجاح

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -25,14 +26,14 @@ class AuthenticatedSessionController extends Controller
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request): RedirectResponse
     {
-        Auth::guard('web')->logout();
+        if ($request->user() && $request->user()->currentAccessToken()) {
+            $request->user()->currentAccessToken()->delete();
+        }
 
-        //$request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return response()->noContent();
+        return redirect()
+            ->route('login')
+            ->with('status', 'تم تسجيل الخروج بنجاح');
     }
 }
